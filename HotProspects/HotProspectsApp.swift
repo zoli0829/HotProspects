601bdbf586f3746c0bf314a48a8db3e8371d27ec
zoli0829 <zoli0829@gmail.com>
2025-05-17 09:34:55 +0100

    Initial commit

---
+++ HotProspects/HotProspectsApp.swift
@@ -0,0 +1,17 @@
+//
+//  HotProspectsApp.swift
+//  HotProspects
+//
+//  Created by Zoltan Vegh on 17/05/2025.
+//
+
+import SwiftUI
+
+@main
+struct HotProspectsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
