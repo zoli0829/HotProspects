65be5fc728dc5beaa9a6a7b60750b3d3d4ed439b
zoli0829 <zoli0829@gmail.com>
2025-05-19 06:11:15 +0100

    finished day 81

--- HotProspects/ContentView.swift
+++ HotProspects/ContentView.swift
@@ -5,33 +5,14 @@
 //  Created by Zoltan Vegh on 17/05/2025.
 //
 
+import SamplePackage
 import SwiftUI
 
 struct ContentView: View {
-    @State private var backgroundColor: Color = .red
+    
     
     var body: some View {
-        VStack {
-            Text("Hello, World!")
-                .padding()
-                .background(backgroundColor)
-            
-            Text("Change Color")
-                .padding()
-                .contextMenu{
-                    Button("Red", systemImage: "checkmark.circle.fill") {
-                        self.backgroundColor = .red
-                    }
-                    
-                    Button("Blue") {
-                        self.backgroundColor = .blue
-                    }
-                    
-                    Button("Green") {
-                        self.backgroundColor = .green
-                    }
-                }
-        }
+        
     }
 }
 
